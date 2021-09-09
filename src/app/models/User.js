05fbe2adb00329d82b7f152a50f97544a9f0e266
vagnersampaio <vagnersampaio92@gmail.com>
2021-09-09 16:30:43 -0300

    corrigido o bug na config do sequelize

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,17 +1,33 @@
 const bcrypt = require('bcryptjs')
-const  jwt=require ('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 
 
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User',{
-        name:DataTypes.STRING,
-        email:DataTypes.STRING,
-        password:DataTypes.VIRTUAL,
+    const User = sequelize.define('User', {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.VIRTUAL,
         password_hash: DataTypes.STRING,
     },
-   )
-    
-    
+        {
+            hooks: {
+                beforeSave: async user => {
+                    if (user.password) {
+                        user.password_hash = await bcrypt.hash(user.password, 8)
+                    }
+                }
+            }
+        })
+    User.prototype.checkPassword = function (password) {
+        return bcrypt.compare(password, this.password_hash)
+    }
+    User.prototype.generation = function (email) {
+
+        return jwt.sign({ email }, process.env.SECRET, {
+            expiresIn: 86400
+        })
+    }
+
     return User
 }
